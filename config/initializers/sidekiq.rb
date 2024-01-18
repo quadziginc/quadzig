fcbd2186d72a4eb3a1864e07af02a439b27bcabf
Ubuntu <ubuntu@ip-172-31-32-110.eu-west-2.compute.internal>
2024-01-18 14:11:11 +0000

    Shakapacka+Dockerfile changes

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,13 @@
+print "Testing"
 if ENV['ASSET_PRECOMPILE'].to_i == 0
   # TODO: Eventually, separate the into 2 redis clusters
   # One for general redis ops & one for sidekiq
-
   # TODO: Check if Global is the best way to do this
   # Should be sidekiq pool size + 2
   # Should probably not hardcode this.
   # TODO: change to env var later.
-  REDIS_POOL = ConnectionPool.new(size: 52) { Redis.new(url: ENV['REDIS_URI']) }
-  Sidekiq.configure_server do |config|
+ REDIS_POOL = ConnectionPool.new(size: 52) { Redis.new(url: ENV['REDIS_URI']) }
+ Sidekiq.configure_server do |config|
     config.redis = REDIS_POOL
     config.logger.formatter = Sidekiq::Logger::Formatters::JSON.new
   end
--- dockerfile.app
+++ dockerfile.app
@@ -9,7 +9,7 @@ RUN apk update
 RUN apk add --no-cache build-base
 
 RUN apk add nodejs npm
-RUN apk add --update nodejs=20.10.0-r1
+RUN apk add --update nodejs=20.11.0-r0
 
 RUN apk add curl yarn postgresql-dev build-base tzdata git
 
@@ -19,6 +19,7 @@ RUN gem install pg -v '1.5.4' -- --with-cflags="-Wno-error=implicit-function-dec
 RUN yarn install
 
 RUN bundle install -V
+RUN rails webpacker:install
 RUN rails assets:clean
 RUN rails assets:precompile
 RUN rails webpacker:compile
